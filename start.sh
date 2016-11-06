530875515b348313facb5a0d7732a8c524427a1e
Erik Meitner <github@wanderings.us>
2016-11-05 22:03:00 -0500

    use --no-http2 option to silence 'ALPN support missing' error

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . ./venv/bin/activate 
-#mitmproxy -T --host -p 4432   -s /opt/1-1e100/cache.py -e -v
-mitmdump -T --host -p 4432   -s /opt/1-1e100/cache.py  -v
+#mitmproxy -T --host -p 4432   -s /opt/1-1e100/cache.py -e -v --no-http2
+mitmdump -T --host -p 4432   -s /opt/1-1e100/cache.py  -v --no-http2
