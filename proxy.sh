5d596a7d0b5916e5ee695c3c0a0fa758d5cfa378
Erik Meitner <github@wanderings.us>
2016-11-14 21:01:40 -0600

    rename, part2

--- proxy.sh
+++ proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . ./venv/bin/activate 
-#mitmproxy -T --host -p 4432   -s /opt/1-1e100/cache.py -e -v --no-http2
-mitmdump -T --host -p 4432   -s /opt/1-1e100/cache.py  -v --no-http2
+#mitmproxy -T --host -p 4432   -s /opt/1-1e100/proxy.py -e -v --no-http2
+mitmdump -T --host -p 4432   -s /opt/1-1e100/proxy.py  -v --no-http2
