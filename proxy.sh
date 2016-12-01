fd70d16b844a520e187168ea3562612d056f6151
Erik Meitner <github@wanderings.us>
2016-12-01 16:02:31 -0600

    use no-upstream-cert so no traffic to target server is generated

--- proxy.sh
+++ proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-. ./venv/bin/activate 
-#mitmproxy -T --host -p 4432   -s /opt/1-1e100/proxy.py -e -v --no-http2
-mitmdump -T --host -p 4432   -s /opt/1-1e100/proxy.py  -v --no-http2
+. ./venv/bin/activate
+#mitmproxy -T --host -p 4432   -s /opt/1-1e100/proxy.py -e -v --no-http2 --no-upstream-cert
+mitmdump -T --host -p 4432   -s /opt/1-1e100/proxy.py  -v --no-http2 --no-upstream-cert
